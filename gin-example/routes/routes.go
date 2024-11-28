735e5715d44ec955461e7807b08f1e0a295d7bc7
Mate Hajnal <hajnalmt@gmail.com>
2024-11-28 16:01:59 +0100

    Update and deleting event for gin-example.

--- gin-example/routes/routes.go
+++ gin-example/routes/routes.go
@@ -6,4 +6,6 @@ func RegisterRoutes(server *gin.Engine) {
 	server.GET("/events", getEvents)
 	server.GET("/events/:id", getEvent) // /events/1, /events/5
 	server.POST("/events", createEvent)
+	server.PUT("/events/:id", updateEvent)
+	server.DELETE("/event/:id", deleteEvent)
 }
