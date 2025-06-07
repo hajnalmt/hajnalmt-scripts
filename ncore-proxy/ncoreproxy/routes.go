2835f5a140b4daa6eb53c8e0fb6db6c7b6b3f2d9
Hajnal Mate <hajnalmt@gmail.com>
2025-06-07 14:45:47 +0200

    Adding ncore-proxy routes...

---
+++ ncore-proxy/ncoreproxy/routes.go
@@ -0,0 +1,18 @@
+package ncoreproxy
+
+import (
+	"net/http"
+)
+
+// RegisterRoutes wires all route handlers to the default HTTP mux.
+func RegisterRoutes(client *http.Client) {
+	http.HandleFunc("/proxy-static/", func(w http.ResponseWriter, r *http.Request) {
+		StaticProxy().ServeHTTP(w, r)
+	})
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		MainProxy(client).ServeHTTP(w, r)
+	})
+
+	http.HandleFunc("/exit.php", HandleBlockedLogout)
+}
